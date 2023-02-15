e04d9d8bf10b95bcd5ae953158c0fd1f33241313
Luan <107047713+LuanPonick@users.noreply.github.com>
2023-02-15 18:20:31 -0300

    Add files via upload

--- SQLSELECTS.sql
+++ SQLSELECTS.sql
@@ -16,3 +16,13 @@ ON
 	DIS.codigo = COM.codigo_disc;
 
 -- Exercicio 3 
+SELECT ALU.nome, DIS.nome FROM ALUNO AS ALU
+INNER JOIN CURSA AS CUR
+ON CUR.cpf_aluno = ALU.cpf
+INNER JOIN DISCIPLINA AS DIS 
+ON DIS.codigo = CUR.codigo_disc;
+
+-- Exercicio 4
+SELECT PRO.*, DIS.nome FROM DISCIPLINA AS DIS
+INNER JOIN PROFESSOR AS PRO
+ON PRO.matricula = DIS.matricula_prof;
